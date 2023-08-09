9c2afc65f5ce5a94cd1cf0ee9630b6f36e6b975f
andrei1994rus <rusakov.andrei@hotmail.com>
2023-08-09 21:26:07 +0300

    add types

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type errorData = Array<String>; /*string[]*/
+export type errorModal = {
+	message: string;
+	code: string;
+};
+export type dataModal = {
+	name: string;
+};
+export type Props = {
+	params: {
+		id: string;
+	};
+};
